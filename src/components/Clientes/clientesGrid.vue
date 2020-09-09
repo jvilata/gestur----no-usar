92924d0d1dd6fdaf207fa729af877d28e1c635e7
Marta <marta.vilata@hotmail.com>
2020-09-09 13:35:04 +0200

    clientesGrid

---
+++ src/components/Clientes/clientesGrid.vue
@@ -0,0 +1,101 @@
+<template>
+  <q-item class="row q-ma-xs q-pa-xs">
+    <!-- GRID. en row-key ponemos la columna del json que sea la id unica de la fila -->
+    <q-table
+      class="personalGrid-header-table"
+      virtual-scroll
+      :pagination.sync="pagination"
+      :rows-per-page-options="[0]"
+      :virtual-scroll-sticky-size-start="48"
+      row-key="id"
+      :data="value"
+      :columns="columns"
+      table-style="max-height: 60vh; max-width: 96vw"
+    >
+
+      <template v-slot:header="props">
+        <!-- CABECERA DE LA TABLA -->
+        <q-tr :props="props">
+          <q-th
+            v-for="col in props.cols"
+            :key="col.name"
+            :props="props"
+          >
+          <div :style="col.style">
+            {{ col.label }}
+          </div>
+          </q-th>
+        </q-tr>
+      </template>
+
+      <template v-slot:body="props">
+        <q-tr :props="props" :key="`m_${props.row.id}`" @mouseover="rowId=`m_${props.row.id}`">
+          <q-td
+            v-for="col in props.cols"
+            :key="col.name"
+            :props="props"
+          >
+          <div :style="col.style">
+              <div>{{ col.value }}</div>
+          </div>
+           </q-td>
+        </q-tr>
+      </template>
+
+      <template v-slot:bottom>
+        <div>
+          {{ value.length }} Filas
+        </div>
+      </template>
+
+    </q-table>
+  </q-item>
+</template>
+
+<script>
+export default {
+  props: ['value'], // en 'value' tenemos los registrosSeleccionados
+  data () {
+    return {
+      expanded: false,
+      columns: [
+        { name: 'id', align: 'left', label: 'ID', field: 'id' },
+        { name: 'nombre', align: 'left', label: 'Nombre', field: 'nombre', style: 'width: 230px; whiteSpace: normal' },
+        { name: 'telefono', label: 'Teléfono', align: 'left', field: 'telefono', style: 'width: 130px' },
+        { name: 'dni', label: 'DNI', align: 'left', field: 'dni', style: 'width: 100px' },
+        { name: 'nacionalidad', align: 'left', label: 'Nacionalidad', field: 'nacionalidad', style: 'width: 130px; whiteSpace: normal' },
+        { name: 'matricula', align: 'left', label: 'Matrícula', field: 'matricula' },
+        { name: 'pendCobro', align: 'left', label: 'pendCobro', field: 'pendCobro' }
+      ],
+      pagination: { rowsPerPage: 0 }
+    }
+  },
+  methods: {
+    mostrarDatosPieTabla () {
+      return this.value.length + ' Filas'
+    }
+  },
+  mounted () {
+    console.log(this.value)
+  }
+}
+</script>
+<style lang="sass">
+  .personalGrid-header-table
+    .q-table__top,
+    .q-table__bottom,
+    thead tr:first-child th
+      /* bg color is important for th; just specify one */
+      background-color: $indigo-1
+
+    thead tr th
+      position: sticky
+      z-index: 1
+    thead tr:first-child th
+      top: 0
+
+    /* this is when the loading indicator appears */
+    &.q-table--loading thead tr:last-child th
+      /* height of all previous header rows */
+      top: 48px
+</style>
--- src/components/Clientes/clientesMain.vue
+++ src/components/Clientes/clientesMain.vue
@@ -1,6 +1,6 @@
 <template>
     <div style="height: calc(100vh - 113px)">
-      <q-item clickable v-ripple class="q-ma-xs q-pa-xs bg-blue-grey-1 text-grey-8" >
+      <q-item clickable v-ripple @click="expanded = !expanded" class="q-ma-xs q-pa-xs bg-blue-grey-1 text-grey-8" >
         <q-item-section avatar>
           <q-icon name="group" size="md"/>
         </q-item-section>
@@ -14,7 +14,16 @@
           @click="$emit('close')" flat round dense icon="close"/>
         </q-item-section>
       </q-item>
-
+        <q-dialog v-model="expanded"  >
+        <!-- Componente de filtro -->
+        <clientesFilter
+            @close="expanded = !expanded"
+        />
+        </q-dialog>
+      <!-- Tabla de resultados de busqueda -->
+      <clientesGrid
+        v-model="registrosSeleccionados"
+        />
     </div>
 </template>
 
@@ -26,33 +35,31 @@ export default {
 
   data () {
     return {
-      nomFormulario: 'Clientes',
-      expanded: false
+      nomFormulario: 'Lista de Clientes',
+      expanded: false,
+      registrosSeleccionados: [
+        { // Array de Objetos (Clientes)
+          id: '1',
+          nombre: 'Marta Vilata',
+          telefono: '+34674078554',
+          dni: '21793010G',
+          nacionalidad: 'ES',
+          matricula: '1376GTJ',
+          pendCobro: '1/3/2020'
+        }
+      ]
     }
   },
-
   methods: {
     ...mapActions('login', ['desconectarLogin'])
   },
 
   destroyed () {
     this.$emit('changeTab', { idTab: this.value.idTab, filterRecord: this.filterRecord })
+  },
+  components: {
+    clientesFilter: require('components/Clientes/clientesFilter.vue').default,
+    clientesGrid: require('components/Clientes/clientesGrid.vue').default
   }
 }
 </script>
-
-<style lang="scss">
-//   .fullScreen {
-//     height: calc(100vh - 190px);
-//     overflow: hidden;
-//   }
-
-//   .sqScreen {
-//     height: calc(100vh - 124px);
-//     overflow: hidden;
-//   }
-
-//   .android {
-//     height: calc(100vh - 113px);
-//   }
-</style>
